53ec8593c5072abdd52547ebd03ade29e0f49b83
Robins Gupta <robinskumar73@gmail.com>
2014-02-25 01:38:15 +0530

    "Color Picker added"

--- other projects/WpfApplication6/WpfApplication6/Window1.py
+++
@@ -1,8 +0,0 @@
-import wpf
-import clr
-clr.AddReference("System")
-from System.Windows import Window
-
-class Window1(Window):
-    def __init__(self):
-        wpf.LoadComponent(self, 'Window1.xaml')
--- other projects/WpfApplication6/WpfApplication6/Window2.py
+++
@@ -1,7 +0,0 @@
-import wpf
-
-from System.Windows import Window
-
-class Window2(Window):
-    def __init__(self):
-        wpf.LoadComponent(self, 'Window2.xaml')
